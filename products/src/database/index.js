5a804baf5275557030da67c0571ea8b8e4b1e28a
sunilsalat <sunilsalat@yahoo.com>
2023-02-09 14:34:05 +0530

    second commit

--- products/src/database/index.js
+++ products/src/database/index.js
@@ -2,6 +2,5 @@
 module.exports = {
     databaseConnection: require('./connection'),
     ProductRepository: require('./repository/product-repository'),
-    CustomerRepository: require('./repository/customer-repository'),
-    ShoppingRepository: require('./repository/shopping-repository')
+    
 }
